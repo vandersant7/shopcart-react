a4263d537385371ee2c5cbfb340102d4cc00245d
Evandro Santos <vandersant7@gmail.com>
2026-03-27 14:27:13 -0300

    feat: refactor Checkout.jsx to use an array for fields involves defining a configuration array of fields object and mapping over it to generate JSX.

--- src/features/checkout/Pages/Checkout.jsx
+++ src/features/checkout/Pages/Checkout.jsx
@@ -1,24 +1,49 @@
-import { Box, Typography } from "@mui/material";
-import { useAuth } from "../../../context/useAuth";
-import { Navigate } from "react-router-dom";
-
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { useAuth } from "../../../context/AuthContext/useAuth";
+import { Navigate, useNavigate } from "react-router-dom";
+import CheckoutForm from "../components/CheckoutForm";
+import OrderSummary from "../components/OrderSummary";
 export default function Checkout() {
   const { user } = useAuth();
+  const navigate = useNavigate();
 
-  // proteção da rota
+  // proteção
   if (!user) {
     return <Navigate to="/login" state={{ from: "/checkout" }} replace />;
   }
 
+  const handleFinish = () => {
+    alert("Pedido finalizado com sucesso! 🎉");
+    navigate("/");
+  };
+
   return (
-    <Box sx={{ p: 4, mt: 10 }}>
-      <Typography variant="h4" mb={2}>
+        <Box sx={{ p: 4, mt: 10 }}>
+      <Typography variant="h4" mb={3}>
         Checkout
       </Typography>
 
-      <Typography>
-        Aqui será o fluxo de finalização da compra.
-      </Typography>
+      <Grid container spacing={3}>
+        {/* Formulário */}
+        <Grid item xs={12} md={8}>
+          <CheckoutForm />
+        </Grid>
+
+        {/* Resumo */}
+        <Grid item xs={12} md={4}>
+          <OrderSummary />
+
+          <Button
+            variant="contained"
+            size="large"
+            fullWidth
+            sx={{ mt: 2 }}
+            onClick={handleFinish}
+          >
+            Finalizar Pedido
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
